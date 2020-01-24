e3e74b231fd54483e62898ecb63d286f98c1e385
Cho Dionysus Roy <chodiony@teach.cs.toronto.edu>
2020-01-23 22:33:55 -0500

    working: benford_helpers / bugs: benford - num is strange when length = base

--- a1/benford/benford.c
+++ a1/benford/benford.c
@@ -14,6 +14,29 @@ int main(int argc, char **argv) {
         fprintf(stderr, "benford position [datafile]\n");
         return 1;
     }
+   
+    int i;
+    
+    // initialize tally
+    int tally[BASE];
+    for (i = 0; i < BASE; i++){
+        tally[i] = 0;
+    } 
+    
+    // tally input
+    
+    int pos = strtol(argv[1], NULL, 10);
+    int num = strtol(argv[2], NULL, 10);
 
-    // TODO: Implement.
+    printf("pos: %d\n", pos);
+    printf("num: %d\n", num);
+    printf("str: %s\n", argv[2]);
+
+    add_to_tally(num, pos, tally);
+            
+    // print tally
+    for (i = 0; i < BASE; i++){
+        printf("%ds: %d\n", i, tally[i]);
+    }
+
 }
--- a1/benford/benford_helpers.c
+++ a1/benford/benford_helpers.c
@@ -3,21 +3,30 @@
 #include "benford_helpers.h"
 
 int count_digits(int num) {
-    // TODO: Implement.
-    return 0;
+    int count = 0;
+    for (int k = num; k > 0; k /= BASE) {
+        count++;
+    }
+    return count;
 }
 
 int get_ith_from_right(int num, int i) {
-    // TODO: Implement.
-    return 0;
+    for (int j = 0; j < i; j++) {
+        num /= 10; 
+    } 
+    return num % 10;
 }
 
 int get_ith_from_left(int num, int i) {
-    // TODO: Implement.
-    return 0;
+    int length = count_digits(num);
+
+    printf("length: %d\n", length);    
+    return get_ith_from_right(num, length - (i + 1));
 }
 
 void add_to_tally(int num, int i, int *tally) {
-    // TODO: Implement.
+    int digit = get_ith_from_left(num, i);
+    tally[digit]++;
+    printf("digit: %d\n", digit);    
     return;
 }
