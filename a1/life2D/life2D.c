ae7289337920e7f4774794038e9882fda4f839cb
Cho Dionysus Roy <chodiony@teach.cs.toronto.edu>
2020-01-27 20:20:18 -0500

    working: life2D + helpers.  fixed memory leak.

--- a1/life2D/life2D.c
+++ a1/life2D/life2D.c
@@ -6,10 +6,32 @@
 
 int main(int argc, char **argv) {
 
+    // life2D requires 3 input args (+1 for exec name)
     if (argc != 4) {
         fprintf(stderr, "Usage: life2D rows cols states\n");
         return 1;
     }
 
-    // TODO: Implement.
+    // initialize variables from arguments
+    int num_rows = strtol(argv[1], NULL, 10);
+    int num_cols = strtol(argv[2], NULL, 10);
+    int num_states = strtol(argv[3], NULL, 10);
+
+    //make board from redirect by scanning digits directly into board array
+    int *board = malloc(sizeof(int) * (num_rows * num_cols));
+    int i = 0;
+
+    while (scanf("%d", &board[i]) == 1) {
+        i++;
+    }
+
+    // update board state number of times and print
+    for (i = 0; i < num_states; i++) {
+        if (i > 0) {
+            update_state(board, num_rows, num_cols);
+        }
+        print_state(board, num_rows, num_cols);
+    }
+    free(board);
+    return 0;
 }
--- a1/life2D/life2D_helpers.c
+++ a1/life2D/life2D_helpers.c
@@ -10,9 +10,68 @@ void print_state(int *board, int num_rows, int num_cols) {
         }
     }
     printf("\n");
+    return;
+}
+
+int check_neighbors(int *board, int i, int num_rows, int num_cols) {
+
+    // cycle through each neighbor, incremembting count if value is 1
+    int count = 0;
+    for (int m = -1; m < 2; m++) {
+        for (int n = -1; n < 2; n++) {
+            int j = i + m * num_cols + n;
+            if (j != i) {
+                if (board[j] == 1) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    // add new_board values based on life rules
+    if (board[i] == 0) { // current is 0
+        if (count == 2 || count == 3) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else { // current is 1
+        if (count < 2 || count > 3) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
 
 void update_state(int *board, int num_rows, int num_cols) {
-    // TODO: Implement.
+    
+    // board is unchanged if has no interior cells
+    if (num_rows < 3 || num_cols < 3) {
+        return;
+    }
+
+    // create new board with updated values
+    int *new_board = malloc(sizeof(int) * (num_rows * num_cols));
+
+    // scan through non-border elements
+    for (int i = 0; i < num_rows * num_cols; i++) {
+        // check neighbors if not edge
+        if (!(i < num_cols || i > (num_cols * (num_rows - 1) - 1) || 
+                i % num_cols == 0 || i % num_cols == num_cols - 1)) 
+        {
+            new_board[i] = check_neighbors(board, i, num_rows, num_cols);
+        } else { // edge remains same
+            new_board[i] = board[i];
+        }
+    }
+
+    // update board
+    for (int i = 0; i < num_rows * num_cols; i++) {
+        board[i] = new_board[i];
+    }
+    free(new_board);
+
     return;
 }
+
